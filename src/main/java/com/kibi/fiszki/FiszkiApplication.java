de0f8c877da9aec167f28a9e068acb2ff60e6c36
pawlakj <J.Pawlak@tt.com.pl>
2020-03-23 21:43:22 +0100

    Initial commit

---
+++ src/main/java/com/kibi/fiszki/FiszkiApplication.java
@@ -0,0 +1,13 @@
+package com.kibi.fiszki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiszkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiszkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kibi/fiszki/FiszkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibi.fiszki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiszkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
